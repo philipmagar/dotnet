05fa007959e573460f9e35e11a72a64a8a9644b1
philip <philipmagar111@gmail.com>
2024-11-24 09:44:24 +0545

    changes

---
+++ Basic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Swapping values of two variables without using the third variable
+            int first_variable = 5;
+            int second_variable = 10;
+            Console.WriteLine("Before Swapping:");
+            Console.WriteLine("First_variable=" + first_variable);
+            Console.WriteLine("Second_variable=" + second_variable);
+            first_variable = first_variable * second_variable;
+            second_variable = first_variable / second_variable;
+            first_variable = first_variable / second_variable;
+            Console.WriteLine("\nAfter Swapping:");
+            Console.WriteLine("First_variable=" + first_variable);
+            Console.WriteLine("Second_variable=" + second_variable);
+    
+        }
+    }
+}
