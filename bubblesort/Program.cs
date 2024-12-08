6d6b18528834f9996308b20af773dd5bae7fea35
philip <philipmagar111@gmail.com>
2024-12-08 08:42:11 +0545

    Bubblesort assignment

---
+++ bubblesort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubblesort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 14, 19,2,3 ,7,15 };
+            SortAlgo sortAlgo = new SortAlgo();
+
+            Console.WriteLine("Before Sorting:");
+            sortAlgo.displayArray(array);
+
+            sortAlgo.bubbleSort(array);
+
+            Console.WriteLine("After Sorting:");
+            sortAlgo.displayArray(array);
+        }
+    }
+}
